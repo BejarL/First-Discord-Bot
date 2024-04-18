9a63616cf29972aaeaff19858b9c8224653d2ce9
Bejar L <143468066+BejarL@users.noreply.github.com>
2024-04-17 18:26:04 -0700

    First bot test

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require('express');
+
+const app = express();
+
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
---
+++ bot.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const token = process.env.BOT_TOKEN;
+
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ],
+    partials: [Partials.Message, Partials.Channel]
+});
+
+const BOT_PREFIX = '!'
+const MUTE_COMMAND = 'mute'
+
+client.on("ready", () => {
+    console.log("Our bot is ready to go!!");
+});
+
+client.on('messageDelete', msg => {
+    msg.channel.send("Deja de borrar mensajes!")
+})
+
+client.on("messageCreate", msg => {
+    if (msg.content === "ping") {
+        msg.reply("Pong!");
+    }
+    if (msg.content === "pancho") {
+        msg.reply("mamahuevo 🥵")
+    }
+    if (msg.content === `${BOT_PREFIX}${MUTE_COMMAND}`) {
+        muteUser(msg.member)
+    }
+});
+
+function muteUser(member) {
+    msg.member.roles.add("1049547348700827679")
+}
+
+client.login(token);
